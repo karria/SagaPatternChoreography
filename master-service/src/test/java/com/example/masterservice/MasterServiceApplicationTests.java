c1506dda4380f4f6973cd6b4f351dfa1bb4c4f1c
Changhyun Kim <karria0525@gmail.com>
2022-06-21 14:22:44 +0900

    환경세팅 중

---
+++ master-service/src/test/java/com/example/masterservice/MasterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.masterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
